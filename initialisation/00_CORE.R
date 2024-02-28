2fcebb49acf5492aa36c4376f04e2482cb152e68
Emmanuel Chassot <EChassot@iotc.local>
2024-02-28 13:07:43 +0400

    Minor changes

--- initialisation/00_CORE.R
+++ initialisation/00_CORE.R
@@ -12,4 +12,6 @@ source("93_FUNCTIONS.R")
 # Source the scripts
 source("../scripts/01.configure_and_preprocess.R")
 source("01_HISTORICAL_CATCH_ESTIMATES.R")
-source("02_NATIONAL_JURISTIDICTION_MAPS.R")
+source("02_SOCIO_ECONOMIC_INDICATORS.R")
+#source("03_CPC_FEATURES_MAPS.R")
+#source("04_NATIONAL_JURISDICTION_MAPS.R")
