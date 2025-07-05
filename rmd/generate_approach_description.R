1bee0daba6439376d16ec377dd12b953ba895b4b
Emmanuel Chassot <EChassot@iotc.local>
2025-07-05 14:53:14 +0400

    Production of a description paper of the Shiny App

---
+++ rmd/generate_approach_description.R
@@ -0,0 +1,12 @@
+# Libraries
+library(officer)
+library(officedown)
+library(knitr)
+library(rmarkdown)
+
+# General report describing data, assumptions, and results
+render("06_00_Simulation_Tool_Description.Rmd", 
+       output_dir = "../outputs/", 
+       output_file = "IOTC-2025-TCAC15 - Simulation_tool_description.docx"
+)
+
