4f31bdbc2ff0bd1cb656ba101b2646ca5d902c44
manuchassot <manu.chassot@gmail.com>
2025-07-07 16:22:41 +0400

    Improvements to the description paper

--- rmd/generate_approach_description.R
+++ rmd/generate_approach_description.R
@@ -5,10 +5,13 @@ library(knitr)
 library(rmarkdown)
 library(openxlsx)
 library(flextable)
+library(dplyr)
+
+setwd("./rmd")
 
 # General report describing data, assumptions, and results
 render("06_00_Simulation_Tool_Description.Rmd", 
        output_dir = "../outputs/", 
-       output_file = "IOTC-2025-TCAC15 - Simulation_tool_description.docx"
+       output_file = "IOTC-2025-TCAC15-INF02 - Simulation_tool_description.docx"
 )
 
