8452ea1c934f4e18152cae36644b40b8d2e413f4
eblondel <emmanuel.blondel1@gmail.com>
2026-01-19 10:23:42 +0100

    fix bbox

--- server.R
+++ server.R
@@ -575,7 +575,8 @@ server = function(input, output, session) {
     
     # Zoom to selection if we have features
     if (nrow(filtered) > 0) {
-      bb <- do.call(sf::st_bbox, lapply(list(filtered, cpc_admin, cpc_nja), sf::st_bbox))
+      #bb <- do.call(sf::st_bbox, lapply(list(filtered, cpc_admin, cpc_nja), sf::st_bbox))
+      bb <- sf::st_bbox(cpc_nja)
       proxy %>% fitBounds(
         lng1 = as.numeric(bb["xmin"]), lat1 = as.numeric(bb["ymin"]),
         lng2 = as.numeric(bb["xmax"]), lat2 = as.numeric(bb["ymax"])
