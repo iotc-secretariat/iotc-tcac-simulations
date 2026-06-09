38b37b37c7cc2dbcc0ac7cda577587cf62916a2a
eblondel <emmanuel.blondel1@gmail.com>
2026-06-09 09:14:28 +0200

    comment script not used

--- initialisation/91_LIBS_EXTERNAL.R
+++ initialisation/91_LIBS_EXTERNAL.R
@@ -1,29 +1,29 @@
-# Install/load pacman
-if(!require(pacman)){
-  install.packages("pacman")
-  suppressPackageStartupMessages(library(pacman,quietly = TRUE))
-}
-
-# Install/load libraries required for analysis
-pacman::p_load(
-  "bookdown",
-  "flextable",   # Use old version: 0.6.9
-  "ggpubr", 
-  "ggsci", 
-  "knitr", 
-  "officer", 
-  "openxlsx",  
-  "rmarkdown", 
-  "scales", 
-  "tidyverse", 
-  "rnaturalearth", 
-  "rnaturalearthdata", 
-  "sf", 
-  "RDCOMClient", 
-  "data.table", 
-  "DT", 
-  "kableExtra", 
-  "shinyWidgets"
-)
-
-theme_set(theme_bw())
+# # Install/load pacman
+# if(!require(pacman)){
+#   install.packages("pacman")
+#   suppressPackageStartupMessages(library(pacman,quietly = TRUE))
+# }
+# 
+# # Install/load libraries required for analysis
+# pacman::p_load(
+#   "bookdown",
+#   "flextable",   # Use old version: 0.6.9
+#   "ggpubr", 
+#   "ggsci", 
+#   "knitr", 
+#   "officer", 
+#   "openxlsx",  
+#   "rmarkdown", 
+#   "scales", 
+#   "tidyverse", 
+#   "rnaturalearth", 
+#   "rnaturalearthdata", 
+#   "sf", 
+#   "RDCOMClient", 
+#   "data.table", 
+#   "DT", 
+#   "kableExtra", 
+#   "shinyWidgets"
+# )
+# 
+# theme_set(theme_bw())
